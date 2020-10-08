577aa5705009d161d7acdf42fb14ce196e865b71
GaluhMahendra <galuhmahendra3@gmail.com>
2020-10-09 00:18:42 +0700

    first commit

--- app/src/main/java/com/example/aplikasisistemdeteksikebakarankosharapanbunda/Beranda_Activity.java
+++ app/src/main/java/com/example/aplikasisistemdeteksikebakarankosharapanbunda/Beranda_Activity.java
@@ -1,8 +1,11 @@
 package com.example.aplikasisistemdeteksikebakarankosharapanbunda;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TextView;
 
 public class Beranda_Activity extends AppCompatActivity {
 
@@ -10,5 +13,29 @@ public class Beranda_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beranda);
+
+        // Temukan tampilan bilah alat di dalam tata letak aktivitas
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarBeranda);
+        // Menyetel Toolbar untuk bertindak sebagai ActionBar untuk jendela Aktivitas ini.
+        // Pastikan toolbar ada dalam aktivitas dan bukan null
+        setSupportActionBar(toolbar);
+
+        // Menghapus title default
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        // Mengambil akses TextView yang ada di dalam Toolbar
+        TextView mTitle = (TextView) toolbar.findViewById(R.id.textToolbarBeranda);
+
+        // Menampilkan ikon di Toolbar
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.logo);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+    }
+
+    // Ikon menu digelembungkan seperti pada bilah tindakan
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Mengembang menu; ini menambahkan item ke bilah tindakan jika ada.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
--- app/src/main/java/com/example/aplikasisistemdeteksikebakarankosharapanbunda/Splash_Activity.java
+++ app/src/main/java/com/example/aplikasisistemdeteksikebakarankosharapanbunda/Splash_Activity.java
@@ -2,13 +2,26 @@ package com.example.aplikasisistemdeteksikebakarankosharapanbunda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Splash_Activity extends AppCompatActivity {
+    private int waktuSplashScreen=4000; //4000=4 detik
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //setelah loading maka akan langsung berpindah ke activity_beranda
+                Intent Beranda=new Intent(Splash_Activity.this, Beranda_Activity.class);
+                startActivity(Beranda);
+                finish();
+            }
+        },waktuSplashScreen);
     }
 }
